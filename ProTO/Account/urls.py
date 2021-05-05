94394fc6489c6177a958da9b5e61244c3bfcf073
engineerscodes <practicalengineers05@gmail.com>
2021-05-05 23:06:15 +0530

    account app

--- ProTO/Account/urls.py
+++ ProTO/Account/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 
 
 urlpatterns = [
-    path()
+   path('',views.reg,name="reg")
 ]
--- ProTO/Account/views.py
+++ ProTO/Account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import  HttpResponse
+def reg(request):
+
+    return HttpResponse("!!!! Coming Soon !!!")
