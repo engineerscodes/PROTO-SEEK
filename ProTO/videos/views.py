f4d47675f1fb8ffc2d02a675a25547d342f603f5
engineerscodes <practicalengineers05@gmail.com>
2021-05-21 23:43:15 +0530

    homepage

---
+++ ProTO/videos/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,redirect
+
+from django.http import HttpResponse
+# Create your views here.
+
+
+def homepage(request):
+    return HttpResponse(request.user.username)
